24b6d0de16683f7bae2e48916b8c0cefe444a07c
nurulafsar02 <khiladimoshin@gmail.com>
2022-08-15 23:59:04 +0600

    2nd commit

---
+++ login.js
@@ -0,0 +1,13 @@
+//
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    if(email === 'anything@.com' && password === 'secret'){
+        window.location.href = "Bank.html";    
+    }
+    else{
+        alert('pleace input valid id or password')
+    }
+})
