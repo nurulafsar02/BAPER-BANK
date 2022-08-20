231b0e06e16f2e65164f8ee2777c24c326d13176
nurulafsar02 <khiladimoshin@gmail.com>
2022-08-20 21:31:18 +0600

    5th commit

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,10 +1,13 @@
-
+    //step-1
 document.getElementById('submit-btn').addEventListener('click', function () {
+    //step-2
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
+    //step-3
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
-    if(email === 'anything@.com' && password === 'secret'){
+    //step-4
+    if(email === '.com' && password === 'secret'){
         window.location.href = "Bank.html";    
     }
     else{
